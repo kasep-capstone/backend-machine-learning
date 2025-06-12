94e450066923c9d740c7b372de5a917a6a000258
dannyysaputra <84920467+dannyysaputra@users.noreply.github.com>
2025-06-12 21:41:12 +0700

    feat - cors setup

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask
 from flasgger import Swagger
 from routes.recommend import recommend_bp
 import logging
-# from flask_cors import CORS
+from flask_cors import CORS
 
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 swagger = Swagger(app)
-# CORS(app, resources={r"/api/*": {"origins": "*"}})
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 # Register blueprint
 app.register_blueprint(recommend_bp, url_prefix='/api')
