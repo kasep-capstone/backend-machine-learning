2b93c8463148022f14707b635ddbc7a9aff498a1
dannyysaputra <84920467+dannyysaputra@users.noreply.github.com>
2025-06-12 08:29:11 +0700

    feat - health check for koyeb

--- Dockerfile
+++ Dockerfile
@@ -11,8 +11,6 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
     && pip install --no-cache-dir -r requirements.txt \
     && apt-get clean && rm -rf /var/lib/apt/lists/*
 
-
-
 COPY . .
 
 EXPOSE 8080
--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask
 from flasgger import Swagger
 from routes.recommend import recommend_bp
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 swagger = Swagger(app)
 
 # Register blueprint
 app.register_blueprint(recommend_bp, url_prefix='/api')
+
+@app.route('/')
+def index():
+    return 'OK', 200
+
+@app.route('/health')
+def health_check():
+    app.logger.debug("Health check called")
+    return 'OK', 200
 
 if __name__ == '__main__':
     app.run(debug=True)
